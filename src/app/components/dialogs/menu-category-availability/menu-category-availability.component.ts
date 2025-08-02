222d177ece16a0041098b5682af0934473781134
kel <michaelkim.ang@ama.edu.ph>
2025-08-03 02:08:20 +0800

    add menu category availability

--- src/app/components/dialogs/menu-category-availability/menu-category-availability.component.ts
+++ src/app/components/dialogs/menu-category-availability/menu-category-availability.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-menu-category-availability',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggleModule
+  ],
   templateUrl: './menu-category-availability.component.html',
   styleUrl: './menu-category-availability.component.scss'
 })
