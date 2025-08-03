5e45e9c21e6d985ca4e7f8e1e847393200fab986
kel <michaelkim.ang@ama.edu.ph>
2025-08-04 02:13:42 +0800

    add search input in header

--- src/app/components/layout/top-nav/top-nav.component.ts
+++ src/app/components/layout/top-nav/top-nav.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, signal } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import {  MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-top-nav',
@@ -14,7 +14,10 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatBadgeModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   template: `
     <nav class="dash-navbar-header">
@@ -22,10 +25,19 @@ import { MatTooltipModule } from '@angular/material/tooltip';
             <mat-icon>menu</mat-icon>
         </button>
         <span class="spacer"></span>
+        <mat-form-field appearance="outline" class="search-field">
+          <mat-label>Search</mat-label>
+          <input matInput matTooltip="Start Typing to Search">
+          <mat-icon matPrefix>search</mat-icon>
+        </mat-form-field>
+        <span class="spacer"></span>
+        <span class="spacer"></span>
+        <span class="spacer"></span>
+        <span class="spacer"></span>
         <button mat-icon-button class="iconColor" matTooltip="Menu">
             <mat-icon>apps</mat-icon>
         </button>
-        <button mat-icon-button class="iconColor" matTooltip="Notifications">
+        <button mat-icon-button matBadge="7" class="iconColor" matTooltip="Notifications">
             <mat-icon>notifications</mat-icon>
         </button>
         <button mat-icon-button class="iconColor" matTooltip="Account">
@@ -34,40 +46,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     </nav>
     
   `,
-  styles: `
-
-  :host {
-    color: var(--mat-sys-primary);
-    background: var(--mat-sys-primary-container);
-    border-bottom: 1px solid;
-    border-color: var(--mat-sys-outline-variant);
-  }
-
-  .dash-navbar-header{
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    padding: 8px 16px;
-  }
-
-  .spacer {
-    flex-grow: 1;
-  }
-
-  .iconColor {
-    color: var(--mat-sys-primary);
-    margin-left: 2px;
-    margin-right: 15px;
-  }
-
-  @media (min-width: 721px) and (max-width: 959px) {
-    .burger-hidden {
-      display: none !important;
-    }
-  }
-
-
-  `
+  styleUrl: './top-nav.scss'
 })
 export class TopNavComponent {
 
